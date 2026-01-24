3ef676558f0640fab507e0a13bebda56aa5f668b
Mahadevastrologerma108 <mahadevastrologerma108@zohomail.in>
2026-01-24 08:18:07 +0530

    script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,13 @@
-js/script.js
+const menuToggle = document.getElementById("menuToggle");
+const navMenu = document.getElementById("navMenu");
+const dropdown = document.getElementById("servicesDropdown");
+
+/* Mobile menu toggle */
+menuToggle.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
+/* Services dropdown */
+dropdown.addEventListener("click", () => {
+  dropdown.classList.toggle("active");
+});
