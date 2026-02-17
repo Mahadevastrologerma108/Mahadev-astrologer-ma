d790651f1fb0078d9e333498970fd5e927ebbe5a
Mahadevastrologerma108 <256489137+Mahadevastrologerma108@users.noreply.github.com>
2026-02-17 18:26:40 +0545

    Create config.js

---
+++ assets/js/config.js
@@ -0,0 +1,9 @@
+// config.js
+const AppConfig = {
+    // Sirf yahan file name change karna hoga future updates ke liye
+    dataFiles: {
+        events: "data/events-2026.json", 
+        panchang: "data/panchang-feb-2026.json"
+    },
+    currentLanguage: 'en' // Default
+};
