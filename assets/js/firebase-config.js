cce6a65a1961e0c2709deca124f28208ffa9263f
Mahadevastrologerma108 <256489137+Mahadevastrologerma108@users.noreply.github.com>
2026-02-19 12:38:18 +0545

    Update firebase-config.js

--- assets/js/firebase-config.js
+++ assets/js/firebase-config.js
@@ -1,1 +1,16 @@
+// Firebase Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAgcfrzQm6wezgtU5Q5BP8wxXatmoWqYrw",
+  authDomain: "mahadev-astrologer.firebaseapp.com",
+  databaseURL: "https://mahadev-astrologer-default-rtdb.firebaseio.com",
+  projectId: "mahadev-astrologer",
+  storageBucket: "mahadev-astrologer.firebasestorage.app",
+  messagingSenderId: "559664802739",
+  appId: "1:559664802739:web:4285f4dc461f570cc2b9c6"
+};
 
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Database connection string
+const db = firebase.database();
